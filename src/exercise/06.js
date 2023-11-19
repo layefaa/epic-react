4b5955883de5e1df6a876762ff36ee9f84069910
Martin-Luther Obahor <kingm4159@gmail.com>
2023-11-19 13:31:08 +0100

    lesson 6 extra credits - using useRef to access inputelements

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -4,16 +4,17 @@
 import * as React from 'react'
 
 function UsernameForm({onSubmitUsername}) {
+    const inputRef = React.useRef(null)
     const handleSubmit = (event) => {
         event.preventDefault();
-        const username = event.target.elements.username.value;
+        const username = inputRef.current.value;
         onSubmitUsername(username)
     }
     return (
         <form onSubmit={handleSubmit}>
             <div>
                 <label htmlFor={"username"}>Username:</label>
-                <input id={"username"} type="text"/>
+                <input id={"username"} ref={inputRef}  type="text"/>
             </div>
             <button type="submit">Submit</button>
         </form>
